67e24b6e59d39e7d52f4f8fad784a11d0d7ce3d0
Christopher Lee <mathmind@2e.co.kr>
2019-10-31 05:07:38 +0900

    added code to luigi.target

--- src/csci_utils/luigi/target.py
+++ src/csci_utils/luigi/target.py
@@ -1,9 +1,14 @@
 from luigi.local_target import LocalTarget, atomic_file
 from contextlib import contextmanager
+import random
+import io
+from luigi.format import FileWrapper
+
 
 class suffix_preserving_atomic_file(atomic_file):
     def generate_tmp_path(self, path):
-        ...
+        file_extension = '.'.join(path.split('/')[-1].split('.')[1:])
+        return '.'.join(path.split('.')[:-1]) + '-luigi-tmp-%09d' % random.randrange(0, 1e10) + file_extension
 
 
 class BaseAtomicProviderLocalTarget(LocalTarget):
@@ -13,7 +18,18 @@ class BaseAtomicProviderLocalTarget(LocalTarget):
     def open(self, mode='r'):
         # leverage super() as well as modifying any code in LocalTarget
         # to use self.atomic_provider rather than atomic_file
-        ...
+        rwmode = mode.replace('b', '').replace('t', '')
+        if rwmode == 'w':
+            self.makedirs()
+            return super(LocalTarget,self).format.pipe_writer(self.atomic_provider(self.path))
+    
+        elif rwmode == 'r':
+            fileobj = FileWrapper(io.BufferedReader(io.FileIO(self.path, mode)))
+            return super(LocalTarget,self).format.pipe_reader(fileobj)
+
+        else:
+            raise Exception("mode must be 'r' or 'w' (got: %s)" % mode)
+
 
     @contextmanager
     def temporary_path(self):
